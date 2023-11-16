45a4db1f495e0e7e9f1cd872684461e16e9fae51
William Word <weword@gmail.com>
2023-11-16 15:16:08 -0800

    'updated'

--- tutorial2/settings.py
+++ tutorial2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')uvfof!dy+lr#+3tdcwipkfi!@wsnydt-$*l(1+uoi5!_k(0p6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
