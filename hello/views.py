3df6c9dbb3aa01b2f1427bf3949df7992fc5bb9f
weword59 <141146632+weword59@users.noreply.github.com>
2023-11-17 08:31:44 -0800

    Update cookie value

--- hello/views.py
+++ hello/views.py
@@ -23,5 +23,5 @@ def sessfun(request) :
     request.session['num_visits'] = num_visits
     if num_visits > 4 : del(request.session['num_visits'])
     resp = HttpResponse('view count='+str(num_visits))
-    resp.set_cookie('dj4e_cookie', '4ac77841', max_age=1000)
+    resp.set_cookie('dj4e_cookie', 'b7d5f59b', max_age=1000)
     return resp
